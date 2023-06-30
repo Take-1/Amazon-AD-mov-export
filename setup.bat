7440995c6476be86c684a3fbe3ea65147c9843fd
Jonathan Hayward <jonathan.hayward@take1.tv>
2023-06-30 13:39:57 +0100

    bug fix path

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 curl -OL "https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-5.1.2-essentials_build.zip"
 powershell Expand-Archive -Path ffmpeg-5.1.2-essentials_build.zip
 del ffmpeg-5.1.2-essentials_build.zip
-move .\ffmpeg-5.1.2-essentials_build\bin\ffmpeg.exe ffmpeg.exe
+move .\ffmpeg-5.1.2-essentials_build\ffmpeg-5.1.2-essentials_build\bin\ffmpeg.exe ffmpeg.exe
 
 if not exist "input" mkdir input
 if not exist "output" mkdir output
